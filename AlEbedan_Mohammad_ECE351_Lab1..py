786f994fe75aa6e81246bd5544afd3455fe2c01a
aleb9491 <90301006+aleb9491@users.noreply.github.com>
2021-09-08 19:39:02 -0700

    Add files via upload

---
+++ AlEbedan_Mohammad_ECE351_Lab1..py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 18:41:11 2021
+
+@author: aleb9491
+"""
+
+################################################################
+# Mohammad Al Ebedan                                           #
+# ECE331-01                                                    #
+# Lab 1                                                        #
+# 9/9/2021                                                     #
+################################################################
+
+
+#%% comment section
+
+#%% section 1
+
+#In the frist section, we learn how to write a function in term of time.
+
+#%% section 2
+
+#In the second section, we were introduced to how we square a variable in Python and
+# how it is different from other languages. 
+
+#%% section 3
+
+#Third, we were taught how we use Variables that contain multiple values are
+# called lists in Python. For many function used
+#in this lab, we will prefer to use numpy arrays,
+# which operate similarly to lists. 
+ 
+#%% section 4
+ 
+#In part four, it was about how to comment in Python. The number sign "#" is used in Python to make a line of code a comment. Thoroughly
+#commenting our code is encouraged, as it demonstrates us comfortable with the purpose
+#of the various functions used and makes it easier for us to debug our code, while we are testing if the other function works or not. #
+
+#%% section 5
+ 
+#In addition, in Python we will not be able to create a larger array unless we include Python and its packages in order to be able to make a larger array.
+
+#%% section 6
+ 
+# In 351 signals and systems lab, we will be using Indexing lists and arrays a lot in order to use them in some experiment. Thus, we need to learn how to use them in Python.
+
+#%% section 7
+
+#In addition, sometimes in somecases we need to use matrix, so in this part since we learn how to use array, we can defined array as matrix of zeros and ones. 
+
+#%% section 8
+
+#Therefore, since the only way to see signals is through graph, we were intreduced on how to use matlpotlib.pyplot in order to plot in python.
+
+#%% section 9
+
+#Moreover, as electrical Engineering we use both AC signals and impedance, and both AC signals and impedance are convenient to represent and calculate by complex number, so we learned that.
+#However, we need to learn how to use them in Python.
+#As result, we learn complex numbers are defined in Python as follows, with j being the imaginary number.
+
+#%% Question
+
+#1. Which course are you most excited for in your degree? Which course have you enjoyed the
+#most so far? 
+
+# I was very excited amd enjoyed to take ECE101, and 210 because I have leared the most important things that related to my major. 
+
+#2. Leave any feedback on the clarity of the expectations, instructions, and deliverables.
+
+#I thank, it's better to share or show us pictures to guide us to what we have to do in each lab.
+
