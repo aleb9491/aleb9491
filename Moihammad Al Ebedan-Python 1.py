dde4551f830ea614ad3456331d1b7e7cea91a16a
aleb9491 <90301006+aleb9491@users.noreply.github.com>
2021-09-08 17:45:20 -0700

    Add files via upload

---
+++ Moihammad Al Ebedan-Python 1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 12:10:10 2021
+
+@author: aleb9491
+"""
+
+################################################################
+# Mohammad Al Ebedan                                           #
+# ECE331-01                                                    #
+# Lab 0                                                        #
+# 9/9/2021                                                     #
+# for upcoming summer, i'm planning to take some classes.      #
+# I'm in this class to learn something useful for my life.     #
+################################################################
